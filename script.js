fdc9d91cf378d4335f6f873cbeb3c29c10fed3b6
André Lubawski <andrelubawski97@outlook.com>
2022-02-06 18:29:04 -0300

    js aplicado na galeria de imagens(página de detalhes de cada bike)

--- script.js
+++ script.js
@@ -39,3 +39,19 @@ function eventosPerguntas(pergunta){
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//galeria de bikes
+const galeria=document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer=document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event){
+    const img = event.currentTarget;
+    const media =matchMedia('(min-width: 1000px)').matches;
+   if(media){ galeriaContainer.prepend(img);
+}}
+
+function eventosGaleria(img){
+    img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
