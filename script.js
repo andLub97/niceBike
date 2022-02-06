17e356e13177a9fd08e2a88c55dad78012577759
André Lubawski <andrelubawski97@outlook.com>
2022-02-06 17:52:46 -0300

    seção de perguntas frequentes agora possui maior interatividade

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ function ativarProduto(parametro){
     elemento.checked=true;}
 }
 parametros.forEach(ativarProduto);
+
+//perguntas freq
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta=document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa=resposta.classList.contains("ativa");
+    pergunta.setAttribute('aria-expanded', ativa);
+    
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
